37818333b7991f014d24a8ec0b53de6740a2ad8c
zhaoxinfu <xinfu.zhao@tcl.com>
2017-03-02 18:39:41 +0800

    delete some files
    N/A
    
    .
    
    Signed-off-by: zhaoxinfu <xinfu.zhao@tcl.com>

--- app/src/main/java/com/cpic/contacts/Constants.java
+++ app/src/main/java/com/cpic/contacts/Constants.java
@@ -6,8 +6,8 @@ package com.cpic.contacts;
 
 public class Constants {
     //url
-    public static final String BASE_URL = "http://10.115.70.61:8080"; //test
-    public static final String LOGIN_URL = BASE_URL + "/admin/news"; //test
+    public static final String BASE_URL = "https://10.115.70.61:8443/"; //test
+    public static final String LOGIN_URL = BASE_URL + "api/v1/userLogin"; //test
 
 
     //static string
